2cf64885419e24688ae806f8df4f1bc7de1b535c
Artem <Artem@devim.team>
2016-10-24 21:18:12 +0300

    change config name to mtt

--- src/SmsProvider.php
+++ src/SmsProvider.php
@@ -21,12 +21,12 @@ class SmsProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
-        $container['sms.login'] = '';
-        $container['sms.password'] = '';
-        $container['sms.short_code'] = '';
+        $container['mtt.login'] = '';
+        $container['mtt.password'] = '';
+        $container['mtt.short_code'] = '';
 
-        $container['sms.sender'] = function () use ($container) {
-            return new MttSmsSender($container['sms.login'], $container['sms.password'], $container['sms.short_code']);
+        $container['mtt.sender'] = function () use ($container) {
+            return new MttSmsSender($container['mtt.login'], $container['mtt.password'], $container['mtt.short_code']);
         };
     }
 }
